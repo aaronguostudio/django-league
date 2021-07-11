e8d7ff637fa1cc63ab3b1642c42679c810337d64
Aaron Guo <aaronguostudio@gmail.com>
2021-07-11 11:32:41 -0600

    update settings

--- ui/src/service/services.ts
+++ ui/src/service/services.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import { ICompetition, IMember, ITeam } from 'src/interface/common'
 
-export const getMembers = () => axios.get('http://127.0.0.1:8000/teammembers')
-export const getTeams = () => axios.get('http://127.0.0.1:8000/teams')
-export const getCompetitions = () => axios.get('http://127.0.0.1:8000/competitions')
-export const getLeagues = () => axios.get('http://127.0.0.1:8000/leagues')
-export const postMember = (member: IMember) => axios.post('http://127.0.0.1:8000/teammembers-create', member)
-export const postTeam = (team: ITeam) => axios.post('http://127.0.0.1:8000/teams-create', team)
-export const postCompetition = (competition: ICompetition) => axios.post('http://127.0.0.1:8000/competitions-create', competition)
+axios.defaults.baseURL = 'http://localhost:8000/'
+
+export const getMembers = () => axios.get('teammembers')
+export const getTeams = () => axios.get('teams')
+export const getCompetitions = () => axios.get('competitions')
+export const getLeagues = () => axios.get('leagues')
+export const postMember = (member: IMember) => axios.post('teammembers-create', member)
+export const postTeam = (team: ITeam) => axios.post('teams-create', team)
+export const postCompetition = (competition: ICompetition) => axios.post('competitions-create', competition)
